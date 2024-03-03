b5f77b7ad7f67256d43934757ae56ed4dda9cdd6
Jared Tichacek <jared.tichacek@gmail.com>
2024-03-02 22:40:41 -0600

    added font style and navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Recursive } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const recursive = Recursive({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={recursive.className}>
+          <Navbar />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <UserButton />
       <h2>Subscribe to Jared</h2>
     </div>
   );
