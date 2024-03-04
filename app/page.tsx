e5ebfbdc4dcae896382cacfa278d8f8c581a3954
Jared Tichacek <jared.tichacek@gmail.com>
2024-03-03 23:23:38 -0600

    added address fields for search functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import Booking from "@/components/Booking/Booking";
 
 export default function Home() {
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-3">
-        <div className="bg-blue-100">Booking</div>
+        <div className="">
+          <Booking />
+        </div>
         <div className="col-span-2 bg-red-100">Map</div>
       </div>
     </div>
---
+++ components/Booking/Booking.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Autocomplete from "./Autocomplete";
+
+const Booking = () => {
+  const screenHeight =
+    typeof window !== "undefined" ? window.innerHeight * 0.72 : 0;
+  return (
+    <div className="p-5">
+      <h2 className="text-[20px] font-semibold">Booking</h2>
+      <div
+        className="border-[1px] p-5 rounded-md"
+        style={{ height: screenHeight }}
+      >
+        <Autocomplete />
+      </div>
+    </div>
+  );
+};
+
+export default Booking;
