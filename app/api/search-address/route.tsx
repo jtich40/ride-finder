bd5fcbdcc1edb0194f94790f7bb83758d3c00023
Jared Tichacek <jared.tichacek@gmail.com>
2024-03-04 23:24:58 -0600

    implemented route for mapbox suggested search results

---
+++ app/api/search-address/route.tsx
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+const BASE_URL = "https://api.mapbox.com/search/searchbox/v1/suggest";
+
+export async function GET(req: any) {
+  const { searchParams } = new URL(req.url);
+
+  const searchText = searchParams.get("q");
+  const res = await fetch(
+    BASE_URL +
+      "?q=" +
+      searchText +
+      "?language=en&limit=8&session_token=f2ba73bd-2f41-4924-99c5-b038b34fc716&country=US" +
+      "&access_token=" +
+      process.env.MAPBOX_ACCESS_TOKEN,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const searchResult = await res.json();
+  return NextResponse.json({ searchResult });
+}
