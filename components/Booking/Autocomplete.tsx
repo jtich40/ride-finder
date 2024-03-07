8ae9c56cff1ee020a6183c0bd0d7aea0ae506d7d
Jared Tichacek <jared.tichacek@gmail.com>
2024-03-06 22:13:41 -0600

    added autocomplete for destination address field

--- components/Booking/Autocomplete.tsx
+++ components/Booking/Autocomplete.tsx
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from "react";
 
 const Autocomplete = () => {
   const [source, setSource] = useState<any>();
+  const [sourceChange, setSourceChange] = useState<any>(false);
+  const [destinationChange, setDestinationChange] = useState<any>(false);
+
   const [addressList, setAddressList] = useState<any>([]);
+  const [destination, setDestination] = useState<any>();
 
   useEffect(() => {
     const delayDebounce = setTimeout(() => {
       getAddressList();
     }, 1000);
     return () => clearTimeout(delayDebounce);
-  }, [source]);
+  }, [source, destination]);
 
   const getAddressList = async () => {
-    const res = await fetch("/api/search-address?q=" + source, {
+    const query = sourceChange ? source : destination;
+    const res = await fetch("/api/search-address?q=" + query, {
       headers: {
         "Content-Type": "application/json",
       },
@@ -20,7 +25,6 @@ const Autocomplete = () => {
 
     const result = await res.json();
     setAddressList(result);
-    console.log(addressList);
   };
 
   return (
@@ -31,9 +35,12 @@ const Autocomplete = () => {
           type="text"
           className="bg-white p-1 border-[1px] w-full rounded-md outline-none focus:border-yellow-300"
           value={source}
-          onChange={(e) => setSource(e.target.value)}
+          onChange={(e) => {
+            setSource(e.target.value);
+            setSourceChange(true);
+          }}
         />
-        {addressList?.suggestions ? (
+        {addressList?.suggestions && sourceChange ? (
           <div className="shadow-md p-1 rounded-md absolute w-full bg-white">
             {addressList?.suggestions.map((item: any, index: number) => (
               <h2
@@ -42,6 +49,7 @@ const Autocomplete = () => {
                 onClick={() => {
                   setSource(item.full_address);
                   setAddressList([]);
+                  setSourceChange(false);
                 }}
               >
                 {item.full_address}
@@ -55,7 +63,29 @@ const Autocomplete = () => {
         <input
           type="text"
           className="bg-white p-1 border-[1px] w-full rounded-md outline-none focus:border-yellow-300"
+          value={destination}
+          onChange={(e) => {
+            setDestination(e.target.value);
+            setDestinationChange(true);
+          }}
         />
+        {addressList?.suggestions && destinationChange ? (
+          <div className="shadow-md p-1 rounded-md absolute w-full bg-white">
+            {addressList?.suggestions.map((item: any, index: number) => (
+              <h2
+                key={index}
+                className="p-3 hover:bg-gray-100 cursor-pointer"
+                onClick={() => {
+                  setDestination(item.full_address);
+                  setAddressList([]);
+                  setDestinationChange(false);
+                }}
+              >
+                {item.full_address}
+              </h2>
+            ))}
+          </div>
+        ) : null}
       </div>
     </div>
   );
