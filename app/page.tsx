9534240d52bdbf286d14e4c749c549c054c56efb
Jared Tichacek <jared.tichacek@gmail.com>
2024-03-17 22:40:37 -0500

    implemented context to access cost of ride for payment

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,14 @@ import { UserLocationContext } from "@/context/UserLocationContext";
 import { SourceCoordinatesContext } from "@/context/SourceCoordinatesContext";
 import { DestinationCoordinatesContext } from "@/context/DestinationCoordinatesContext";
 import { DirectionDataContext } from "@/context/DirectionDataContext";
+import { SelectedCarAmountContext } from "@/context/SelectedCarAmountContext";
 
 export default function Home() {
   const [userLocation, setUserLocation] = useState<any>();
   const [sourceCoordinates, setSourceCoordinates] = useState<any>([]);
   const [destinationCoordinates, setDestinationCoordinates] = useState<any>([]);
   const [directionData, setDirectionData] = useState<any>([]);
+  const [carAmount, setCarAmount] = useState<any>();
 
   useEffect(() => {
     getUserLocation();
@@ -38,16 +40,20 @@ export default function Home() {
           <DirectionDataContext.Provider
             value={{ directionData, setDirectionData }}
           >
-            <div>
-              <div className="grid grid-cols-1 md:grid-cols-3">
-                <div className="">
-                  <Booking />
-                </div>
-                <div className="col-span-2">
-                  <MapBoxMap />
+            <SelectedCarAmountContext.Provider
+              value={{ carAmount, setCarAmount }}
+            >
+              <div>
+                <div className="grid grid-cols-1 md:grid-cols-3">
+                  <div className="">
+                    <Booking />
+                  </div>
+                  <div className="col-span-2">
+                    <MapBoxMap />
+                  </div>
                 </div>
               </div>
-            </div>
+            </SelectedCarAmountContext.Provider>
           </DirectionDataContext.Provider>
         </DestinationCoordinatesContext.Provider>
       </SourceCoordinatesContext.Provider>
--- components/Booking/Booking.tsx
+++ components/Booking/Booking.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Autocomplete from "./Autocomplete";
 import Cars from "./Cars";
 import Cards from "./Cards";
+import { useRouter } from "next/navigation";
+import { SelectedCarAmountContext } from "@/context/SelectedCarAmountContext";
 
 const Booking = () => {
   //   const screenHeight =
   //     typeof window !== "undefined" ? window.innerHeight * 0.72 : 0;
+  const { carAmount, setCarAmount } = useContext(SelectedCarAmountContext);
+  const router = useRouter();
+
   return (
     <div className="p-5">
       <h2 className="text-[20px] font-semibold">Booking</h2>
@@ -16,7 +21,13 @@ const Booking = () => {
         <Autocomplete />
         <Cars />
         <Cards />
-        <button className="w-full bg-yellow-400 p-1 rounded-md mt-4">
+        <button
+          className={`w-full bg-yellow-400 p-1 rounded-md mt-4 ${
+            !carAmount ? "bg-gray-200" : null
+          } `}
+          //   disabled={!carAmount}
+          onClick={() => router.push("/payment")}
+        >
           Book
         </button>
       </div>
